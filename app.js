204baabb215b586d7b72b1eef9229963e1ad0636
Jordon Frank <Maudemoo@gmail.com>
2018-03-08 18:13:09 -0700

    full functionality to first page

--- app.js
+++ app.js
@@ -1,1 +1,26 @@
 
+
+
+
+
+
+
+fetch('https://lit-fortress-6467.herokuapp.com/object')
+.then(response => response.json())
+.then(data => {
+  console.log(data)
+  var album = data.results
+
+
+  for(i = 0; i < data.results.length; i++){
+  let x = Math.floor(Math.random() * data.results.length);
+  var images =  document.getElementById('img'+ i)
+  let imgs = data.results[x].cover_art
+  images.src = 'images/'+ imgs
+
+
+
+
+  }
+
+});
