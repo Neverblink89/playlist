507d3424444fe1ea1f1d32f9d599595b799272c0
Jordon Frank <Maudemoo@gmail.com>
2018-03-09 18:27:57 -0700

    ugly semi finised code don't judge me jeff

--- app.js
+++ app.js
@@ -1,27 +1,18 @@
-
-
-
-
-
-
-
 fetch('https://lit-fortress-6467.herokuapp.com/object')
-.then(response => response.json())
-.then(data => {
-  console.log(data)
+  .then(response => response.json())
+  .then(data => {
+    console.log(data)
 
-
-
-  for(i = 0; i < data.results.length; i++){
-  let x = Math.floor(Math.random() * data.results.length);
-  var images =  document.getElementById('img'+ i)
-  let imgs = data.results[x].cover_art
-  images.src = 'images/'+ imgs
-
+    for (i = 0; i < data.results.length; i++) {
+      let x = Math.floor(Math.random() * data.results.length);
+      var images = document.getElementById('img' + i)
+      let imgs = data.results[x].cover_art
+      images.src = 'images/' + imgs
 
 
 
 
-  }
 
-});
+    }
+
+  });
